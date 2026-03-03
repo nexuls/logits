086c8c21f2f2f0d139d01ec54c4b7bf153c6383f
Arif Sardar <arifsardar.private@gmail.com>
2026-03-03 18:36:07 +0530

    feat: Place holder Header in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/header";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="relative w-full h-dvh bg-background">
+      <Header placeholder className="relative w-full border-b rounded-none" />
       <main className="">
         <Link href="/p/1" className="text-blue-500 underline">
           Project 1
