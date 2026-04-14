172beb5c4ef78c0bad6cca3f1013065fc75bc2b5
Arif Sardar <arifsardar.private@gmail.com>
2026-04-15 02:52:47 +0530

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -89,7 +89,11 @@ export default async function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, interactive-widget=resizes-content"
         ></meta>
-        <link rel="manifest" href="/manifest.webmanifest" crossOrigin="use-credentials" />
+        <link
+          rel="manifest"
+          href="/manifest.webmanifest"
+          crossOrigin="use-credentials"
+        />
       </head>
 
       <body className="font-geist antialiased">
